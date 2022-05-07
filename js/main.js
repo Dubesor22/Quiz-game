15f269ba21ec08ef1e9bd0b8b0130c04a2447118
Dubesor22 <dubesor22@gmail.com>
2022-05-07 21:04:32 +0200

    logica terminada

--- js/main.js
+++ js/main.js
@@ -4,107 +4,105 @@ $(document).ready(function () {
 
 //Variable Declaration Y DOM
 
-const question = document.querySelector("#question");
-const answers = Array.from(document.querySelectorAll(".answer-text"));
-const questionCounterText = document.querySelector(".counter");
-const scoreText = document.querySelector(".score");
+const questionContainer = document.querySelector("#question");
+const answersContainer = Array.from(document.querySelectorAll(".answer-text"));
+const questionCounterText = document.querySelector("#counter");
+const scoreText = document.querySelector("#score");
 const startButton = document.querySelector(".start-button");
+const modal = document.querySelector(".modal-body");
+const buttonNext = document.querySelector("#btn-next");
 
-let currentQuestion = {};
 let acceptingAnswers = true;
-let questionCounter = 0;
 let score = 0;
+let userAnswers = [];
+
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 10;
 let avaliableQuestions = [];
 
-let questions = [];
-
-fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
-  .then((res) => {
-    return res.json();
-  })
-  .then((loadedQuestions) => {
-    console.log(loadedQuestions.results);
-    questions = loadedQuestions.results.map((loadedQuestion) => {
-      const formattedQuestion = {
-        question: loadedQuestion.question,
-      };
-      const answerChoices = [...loadedQuestion.incorrect_answers];
-      formattedQuestion.answers = Math.floor(Math.random() * 3) + 1;
-      answerChoices.splice(
-        formattedQuestion.answers - 1,
-        0,
-        loadedQuestion.correct_answer
-      );
-      answerChoices.forEach((choice, index) => {
-        formattedQuestion["choice" + (index + 1)] = choice;
-      });
-      return formattedQuestion;
-    });
-    // startGame();
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-//Funcion para empezar el Juego
-startGame = () => {
-  questionCounter = 0;
-  score = 0;
-
-  avaliableQuestions = [...questions];
-  console.log(avaliableQuestions);
-  getNewQuestion();
+const getQuestions = async () => {
+  return fetch(
+    "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple"
+  )
+    .then((res) => res.json())
+    .then((res) => res.results);
 };
 
-const getNewQuestion = () => {
-  if (avaliableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    alert("FIN DEL JUEGO");
-    return;
-  }
-  questionCounter++;
-  questionCounterText.innerText = `${questionCounter} / ${MAX_QUESTIONS}`;
-  const questionIndex = Math.floor(Math.random() * avaliableQuestions.length);
-  currentQuestion = avaliableQuestions[questionIndex];
-  question.innerText = currentQuestion.question;
-  choices.forEach((choice) => {
-    //accede a atributos especiales
-    const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number];
-  });
+//Funcion para empezar el Juego
+const startGame = async () => {
+  score = 0;
+  userAnswers = [];
+  updateScore();
+  const questions = await getQuestions();
 
-  //usamos splice para sacar la pregunta que hemos usado.
-  avaliableQuestions.splice(questionIndex, 1);
-  acceptingAnswers = true;
+  printQuestion(questions[0]);
+  buttonNext.onclick = () => {
+    if (userAnswers.length >= questions.length) {
+      endGame();
+      return;
+    }
+
+    printQuestion(questions[userAnswers.length]);
+  };
+};
+
+//funcion guardar en local storage
+const endGame = () => {
+  const globalScore = JSON.parse(localStorage.getItem("globalScore")) || [];
+  globalScore.push(score);
+  localStorage.setItem("globalScore", JSON.stringify(globalScore));
+  showFinalScore(globalScore);
+};
+
+const validateAnswer = (question, answer) => {
+  if (answer === question.correct_answer) {
+    score++;
+    return true;
+  }
+  return false;
+};
+
+const removeColors = () => {
+  answersContainer.forEach((eachContainer) =>
+    eachContainer.classList.remove("correct", "incorrect")
+  );
+};
+
+const printQuestion = (q) => {
+  removeColors();
+  updateScore();
+  questionContainer.innerText = q.question;
+  const answers = [...q.incorrect_answers];
+  answers.splice(Math.floor(Math.random() * 4), 0, q.correct_answer);
+  answers.forEach((answer, i) => {
+    answersContainer[i].innerText = answer;
+    answersContainer[i].onclick = () => {
+      userAnswers.push(answer);
+      answersContainer.forEach(
+        (eachContainer) => (eachContainer.onclick = undefined)
+      );
+
+      const isCorrect = validateAnswer(q, answer);
+
+      if (isCorrect) {
+        answersContainer[i].classList.add("correct");
+        return;
+      }
+      answersContainer[i].classList.add("incorrect");
+    };
+  });
+};
+
+const updateScore = () => {
+  scoreText.innerText = score;
+  questionCounterText.innerText = userAnswers.length + 1;
+};
+
+const showFinalScore = (globalScore) => {
+  console.log(globalScore);
+  modal.innerText = `Felicidades tu Puntuacion es ${score}, volver a Jugar?`;
+  $("#myModal").modal("show");
+  startGame();
 };
 
 startButton.addEventListener("click", startGame);
-
-//funcion que reconoce el click del elmento de cada respuesta
-choices.forEach((choice) => {
-  choice.addEventListener("click", (argument) => {
-    if (!acceptingAnswers) return;
-
-    acceptingAnswers = false;
-    const selectedChoice = argument.target; //value?
-    const selectedAnswer = selectedChoice.dataset["number"];
-    //cambiar a correcto la clase y aplicarla (incorrecto default)
-    const classToAplly = "incorrect";
-    if (selectedAnswer == currentQuestion.answer) {
-      classToAplly = "correct";
-    }
-    if (classToAplly == "correct") {
-      incrementScore(CORRECT_BONUS);
-    }
-    //anyadir y quitar la clase em 2 segundos
-    selectedChoice.parentElement.classList.add(classToAplly);
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToAplly);
-      getNewQuestion();
-    }, 2000);
-  });
-});
-incrementScore = (num) => {
-  score += num;
-  scoreText.innerText = score;
-};
